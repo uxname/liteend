92dfcc54e6fef3a79d29b85f7b6a8ff266b7ee76
uxname <uxname@gmail.com>
2021-03-18 16:37:03 +0300

    Updated Prisma Added script "prisma:create"

---
+++ prisma/migrations/20210318122957_/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT
+);
