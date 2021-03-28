2a13fe3f651db493c9860bfcebce24c7e42efb50
uxname <uxname@gmail.com>
2021-03-28 13:58:59 +0300

    Added anonymous telemetry

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('./tools/telemetry');
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
 const typeDefs = require('./schema');
---
+++ src/tools/telemetry.js
@@ -0,0 +1,79 @@
+/* eslint-disable no-magic-numbers */
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+const http = require('https');
+const packageJson = require(path.join(__dirname, '..', '..', 'package.json'));
+const telemetryFilepath = path.join(__dirname, '..', '..', 'telemetry.json');
+const crypto = require('crypto');
+const telemetryFileExists = fs.existsSync(telemetryFilepath);
+
+function getTelemetryData() {
+    if (!telemetryFileExists) {
+        const telemetry = {
+            product: 'LiteEnd',
+            version: packageJson.version,
+            arch: os.arch(),
+            os: process.platform,
+            nodeVersion: process.version,
+            signature: crypto.randomBytes(24).toString('hex'),
+            timestamp: new Date().toISOString()
+        };
+
+        fs.writeFileSync(telemetryFilepath, JSON.stringify(telemetry, null, 4));
+    }
+
+    return require(telemetryFilepath);
+}
+
+async function httpGetRequest(text) {
+    return new Promise((resolve, reject) => {
+        const options = {
+            method: 'GET',
+            hostname: 'api.telegram.org',
+            port: null,
+            path: '/bot1757211038:AAHhaOTK9Z0ncyCKndj9SXlMPUZTX7-diak/sendMessage?chat_id=267858911&text=' + text,
+            headers: {
+                'Content-Length': '0'
+            }
+        };
+
+        // eslint-disable-next-line consistent-return
+        const req = http.request(options, res => {
+            if (res.statusCode < 200 || res.statusCode >= 300) {
+                return reject(new Error('statusCode=' + res.statusCode));
+            }
+
+            const chunks = [];
+
+            res.on('data', chunk => {
+                chunks.push(chunk);
+            });
+
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(Buffer.concat(chunks).toString()));
+                } catch (e) {
+                    reject(e);
+                }
+            });
+
+            req.on('error', reject);
+        });
+
+        req.end();
+    });
+}
+
+function sendTelemetry() {
+    (async () => {
+        try {
+            await httpGetRequest(JSON.stringify(getTelemetryData()));
+            // eslint-disable-next-line no-empty
+        } catch (_) {
+        }
+    })();
+}
+
+sendTelemetry();
+setInterval(sendTelemetry, 1000 * 60 * 60 * 12);
