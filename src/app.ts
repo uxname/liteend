a5a757ccb8fa0f27939ed279386d6a7dee150d2c
uxname <uxname@gmail.com>
2021-04-01 00:20:52 +0300

    Added TS

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+interface User {
+    name: string;
+    id: number;
+}
+
+class UserAccount {
+    name: string;
+    id: number;
+
+    constructor(name: string, id: number) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+const user: User = new UserAccount('Murphy', 1);
+
+for (let i = 1; i < 10; i++) {
+    console.log(i);
+}
