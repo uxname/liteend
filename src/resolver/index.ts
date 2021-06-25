1901e95e36a88211654af22d6f71546d101ab6e9
uxname <uxname@gmail.com>
2021-06-25 23:15:59 +0300

    Added date scalar

--- src/resolver/index.ts
+++ src/resolver/index.ts
@@ -1,9 +1,19 @@
 import Query from './query';
 import Mutation from './mutation';
 import {Resolvers} from '../generated/graphql_api';
+import {GraphQLScalarType} from 'graphql';
 
 const resolvers: Resolvers = {
     Query: Query.Query,
-    Mutation: Mutation.Mutation
+    Mutation: Mutation.Mutation,
+    Date: new GraphQLScalarType({
+        name: 'Date',
+        parseValue(value) {
+            return new Date(value);
+        },
+        serialize(value) {
+            return value.toISOString();
+        }
+    })
 };
 export default resolvers;
--- src/tools/mocks.ts
+++ src/tools/mocks.ts
@@ -4,6 +4,7 @@ import _ from 'lodash';
 export const mocks = {
     Int: () => _.sample([1111, 2222, 3333, 4444, 1234, 1, 0, -123]),
     Float: () => _.sample([0.123, 1.111, 777.123, -123.456]),
-    String: () => _.sample(['Hello world', 'lorem ipsum', 'Just a test string', 'test_str', 'test'])
+    String: () => _.sample(['Hello world', 'lorem ipsum', 'Just a test string', 'test_str', 'test']),
+    Date: () => _.sample([new Date(), new Date('2022-12-31T23:59:59.999Z'), new Date('2020-06-25T18:31:34.483Z'), new Date('1999-01-12T14:14:14.114Z')])
 };
 
