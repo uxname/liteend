f601a9f80de4bc8f28131e3e4faa84f76f2ab99a
uxname <uxname@gmail.com>
2021-09-04 02:18:01 +0300

    Implemented logging merged graphql requests

--- src/tools/RequestLogger.ts
+++ src/tools/RequestLogger.ts
@@ -2,24 +2,36 @@ import {getLogger} from './Logger';
 import * as graphql from 'graphql';
 import express, {RequestHandler} from 'express';
 import {AddressInfo} from 'net';
+import {Source} from 'graphql/language/source';
 
 const log = getLogger('graphql_request_logger');
 
 export default class RequestLogger {
     public static logGraphQL(httpRequest: express.Request): void {
-        const result: string[] = [];
-        const request = graphql.parse(httpRequest.body.query);
-        request.definitions.forEach(definition => {
-            if ('selectionSet' in definition) {
-                definition.selectionSet.selections.forEach(selection => {
-                    if ('name' in selection) {
-                        result.push(selection.name.value);
-                    }
-                });
-            }
-        });
+        function logBody(query: string | Source, isMerged: boolean) {
+            const result: string[] = [];
+            const request = graphql.parse(query);
+            request.definitions.forEach(definition => {
+                if ('selectionSet' in definition) {
+                    definition.selectionSet.selections.forEach(selection => {
+                        if ('name' in selection) {
+                            result.push(selection.name.value);
+                        }
+                    });
+                }
+            });
 
-        log.trace(`GraphQL: [${result.join(', ')}]`);
+            const isMergedPrefix = isMerged ? ' (merged request)' : '';
+            log.trace(`GraphQL${isMergedPrefix}: [${result.join(', ')}]`);
+        }
+
+        if (Array.isArray(httpRequest.body)) {
+            for (const body of httpRequest.body) {
+                logBody(body.query, true);
+            }
+        } else {
+            logBody(httpRequest.body.query, false);
+        }
     }
 
     public static logHttp: RequestHandler = (request, _, next) => {
