7194c7bd943566277bc057d78bbb98d7f2097700
uxname <uxname@gmail.com>
2021-10-22 02:57:35 +0300

    Improved jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require('./package.json');
+
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'node'
+    testEnvironment: 'node',
+    displayName: {
+        name: packageJson.name,
+        color: 'blue'
+    }
 };
