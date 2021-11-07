bd85baeed1440a4104339a31e592542303a86246
uxname <uxname@gmail.com>
2021-11-07 17:12:18 +0300

    Improved RequestLogger.ts

--- src/tools/RequestLogger.ts
+++ src/tools/RequestLogger.ts
@@ -1,5 +1,4 @@
 import {getLogger} from './Logger';
-import * as graphql from 'graphql';
 import express, {RequestHandler} from 'express';
 import {AddressInfo} from 'net';
 import {Source} from 'graphql/language/source';
@@ -9,20 +8,8 @@ const log = getLogger('graphql_request_logger');
 export default class RequestLogger {
     public static logGraphQL(httpRequest: express.Request): void {
         function logBody(query: string | Source, isMerged: boolean) {
-            const result: string[] = [];
-            const request = graphql.parse(query);
-            request.definitions.forEach(definition => {
-                if ('selectionSet' in definition) {
-                    definition.selectionSet.selections.forEach(selection => {
-                        if ('name' in selection) {
-                            result.push(selection.name.value);
-                        }
-                    });
-                }
-            });
-
             const isMergedPrefix = isMerged ? ' (merged request)' : '';
-            log.trace(`GraphQL${isMergedPrefix}: [${result.join(', ')}]`);
+            log.trace(`GraphQL${isMergedPrefix}: ${query}`);
         }
 
         if (Array.isArray(httpRequest.body)) {
--- src/tools/Telemetry.ts
+++ src/tools/Telemetry.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import * as https from 'https';
 import crypto from 'crypto';
 import os from 'os';
