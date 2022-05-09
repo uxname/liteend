2e4edad74ca780be13cb6b2dcfdfbbbbe1625ecf
uxname <uxname@gmail.com>
2022-05-09 14:10:49 +0300

    Added Json type mock resolver

--- src/core/common/mocks.service.ts
+++ src/core/common/mocks.service.ts
@@ -14,6 +14,14 @@ export const mocksService = {
             };
         }
     },
+    Json: () => ({
+        floatData: 1.23456,
+        exampleData: {
+            id: 1,
+            name: 'test_name',
+            description: 'test_description'
+        }
+    }),
     Int: () => _.sample([1111, 2222, 3333, 4444, 1234, 1, 0, -123]),
     Float: () => _.sample([0.123, 1.111, 777.123, -123.456]),
     String: () => _.sample(['Hello world', 'lorem ipsum', 'Just a test string', 'test_str', 'test']),
