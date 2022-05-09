481d66e5848a3484bb7eb25527c4541678d87071
uxname <uxname@gmail.com>
2022-05-09 13:45:10 +0300

    Added roots parameter to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ const packageJson = require('./package.json');
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+    roots: ['<rootDir>/src'],
     preset: 'ts-jest',
     testEnvironment: 'node',
     displayName: {
