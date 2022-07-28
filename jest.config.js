780b661e23afbc6534187b71eaea02f233d306fb
uxname <uxname@gmail.com>
2022-07-29 02:28:23 +0300

    Set jest timeout

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ const packageJson = require('./package.json');
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
     roots: ['<rootDir>/src'],
+    testTimeout: 30000,
     preset: 'ts-jest',
     testEnvironment: 'node',
     displayName: {
