b330234f88ad158df576a77e991cf8fd9b9d07c7
uxname <uxname@gmail.com>
2022-11-10 22:52:40 +0300

    Moved logger config to logger.service.ts

--- src/modules/common/logger.service.ts
+++ src/modules/common/logger.service.ts
@@ -1,8 +1,121 @@
 import log4js, {Logger} from 'log4js';
-import config from '../../config/config';
 import cluster from 'cluster';
+import path from 'path';
 
-log4js.configure(config.server.loggerConfig);
+// eslint-disable-next-line no-magic-numbers
+const MAX_BACKUP_SIZE = 5 * 1024 * 1024; // maximum size (in bytes) for the log file.
+const MAX_BACKUP_COUNT = 100; // maximum number of log files.
+
+log4js.configure({
+    appenders: {
+        out: {type: 'stdout'},
+        fileOutAll: {
+            type: 'file',
+            filename: path.join(__dirname, '/../../../data/logs/all/logs_all.log'),
+            maxLogSize: MAX_BACKUP_SIZE,
+            backups: MAX_BACKUP_COUNT,
+            compress: true
+        },
+        fileOutAllFilter: {
+            type: 'logLevelFilter',
+            level: 'trace',
+            appender: 'fileOutAll'
+        },
+        fileOutTrace: {
+            type: 'file',
+            filename: path.join(__dirname, '/../../../data/logs/trace/logs_trace.log'),
+            maxLogSize: MAX_BACKUP_SIZE,
+            backups: MAX_BACKUP_COUNT,
+            compress: true
+        },
+        fileOutTraceFilter: {
+            type: 'logLevelFilter',
+            level: 'trace',
+            maxLevel: 'trace',
+            appender: 'fileOutTrace'
+        },
+        fileOutDebug: {
+            type: 'file',
+            filename: path.join(__dirname, '/../../../data/logs/debug/logs_debug.log'),
+            maxLogSize: MAX_BACKUP_SIZE,
+            backups: MAX_BACKUP_COUNT,
+            compress: true
+        },
+        fileOutDebugFilter: {
+            type: 'logLevelFilter',
+            level: 'debug',
+            maxLevel: 'debug',
+            appender: 'fileOutDebug'
+        },
+        fileOutInfo: {
+            type: 'file',
+            filename: path.join(__dirname, '/../../../data/logs/info/logs_info.log'),
+            maxLogSize: MAX_BACKUP_SIZE,
+            backups: MAX_BACKUP_COUNT,
+            compress: true
+        },
+        fileOutInfoFilter: {
+            type: 'logLevelFilter',
+            level: 'info',
+            maxLevel: 'info',
+            appender: 'fileOutInfo'
+        },
+        fileOutWarn: {
+            type: 'file',
+            filename: path.join(__dirname, '/../../../data/logs/warn/logs_warn.log'),
+            maxLogSize: MAX_BACKUP_SIZE,
+            backups: MAX_BACKUP_COUNT,
+            compress: true
+        },
+        fileOutWarnFilter: {
+            type: 'logLevelFilter',
+            level: 'warn',
+            maxLevel: 'warn',
+            appender: 'fileOutWarn'
+        },
+        fileOutError: {
+            type: 'file',
+            filename: path.join(__dirname, '/../../../data/logs/error/logs_error.log'),
+            maxLogSize: MAX_BACKUP_SIZE,
+            backups: MAX_BACKUP_COUNT,
+            compress: true
+        },
+        fileOutErrorFilter: {
+            type: 'logLevelFilter',
+            level: 'error',
+            maxLevel: 'error',
+            appender: 'fileOutError'
+        },
+        fileOutFatal: {
+            type: 'file',
+            filename: path.join(__dirname, '/../../../data/logs/fatal/logs_fatal.log'),
+            maxLogSize: MAX_BACKUP_SIZE,
+            backups: MAX_BACKUP_COUNT,
+            compress: true
+        },
+        fileOutFatalFilter: {
+            type: 'logLevelFilter',
+            level: 'fatal',
+            maxLevel: 'fatal',
+            appender: 'fileOutFatal'
+        }
+    },
+    categories: {
+        default: {
+            appenders: [
+                'out',
+                'fileOutAllFilter',
+                'fileOutTraceFilter',
+                'fileOutDebugFilter',
+                'fileOutInfoFilter',
+                'fileOutWarnFilter',
+                'fileOutErrorFilter',
+                'fileOutFatalFilter'
+            ],
+            level: 'trace'
+        }
+    }
+});
 
 /**
  * Add warning on using default console object
@@ -39,7 +152,7 @@ export function getLogger(name: string): Logger {
     if (!name) {
         throw Error('Logger name is required');
     }
-    const clusterId = cluster.isMaster ? 'master' : `worker ${cluster?.worker?.id}`;
+    const clusterId = cluster.isPrimary ? 'master' : `worker ${cluster?.worker?.id}`;
     return log4js.getLogger(`[${name}] [${clusterId}]`);
 }
 
