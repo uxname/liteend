f50b1351ed539058684a5278de7f7040085ad1c0
uxname <uxname@gmail.com>
2023-04-21 15:51:16 +0300

    app.enableCors();

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
+  app.enableCors();
   app.use(
     helmet({
       crossOriginEmbedderPolicy: false,
