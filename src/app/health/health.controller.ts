d313e26cdc3504d69987c10e482fe1e55fd1026e
uxname <uxname@gmail.com>
2024-06-04 00:00:59 +0300

    Added healthcheck for redis

--- src/app/health/health.controller.ts
+++ src/app/health/health.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import express from 'express';
+import Redis from 'ioredis';
 
 import { Logger } from '@/common/logger/logger';
 import { PrismaService } from '@/common/prisma/prisma.service';
@@ -7,31 +9,55 @@ import { PrismaService } from '@/common/prisma/prisma.service';
 @Controller('health')
 export class HealthController {
   private readonly logger: Logger = new Logger(HealthController.name);
-  constructor(private readonly prisma: PrismaService) {}
+  private readonly client: Redis;
+
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+  ) {
+    const redisHost = this.configService.get<string>('REDIS_HOST');
+    const redisPort = this.configService.get<number>('REDIS_PORT');
+    const redisPassword = this.configService.get<string>('REDIS_PASSWORD');
+    const redisUrl = `redis://${redisHost}:${redisPort}?password=${redisPassword}`;
+    this.client = new Redis(redisUrl);
+  }
 
   @Get()
   async getHealth(@Res() response: express.Response): Promise<unknown> {
+    const databaseOnline = await this.checkDatabase();
+    const redisOnline = await this.checkRedis();
+
+    const status =
+      databaseOnline && redisOnline
+        ? HttpStatus.OK
+        : HttpStatus.SERVICE_UNAVAILABLE;
+    return response.status(status).json({
+      status: status === HttpStatus.OK ? 'ok' : 'error',
+      info: {
+        serverTime: new Date().toISOString(),
+        database: { status: databaseOnline ? 'ok' : 'error' },
+        redis: { status: redisOnline ? 'ok' : 'error' },
+      },
+    });
+  }
+
+  async checkDatabase(): Promise<boolean> {
     try {
       await this.prisma.$executeRaw`SELECT 1`;
-      return response.status(HttpStatus.OK).json({
-        status: 'ok',
-        info: {
-          serverTime: new Date().toISOString(),
-          database: {
-            status: 'ok',
-          },
-        },
-      });
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-      this.logger.error(error.message, error.stack);
-      return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-        status: 'error',
-        info: {
-          serverTime: new Date().toISOString(),
-          error: error.message,
-        },
-      });
+      return true;
+    } catch (error) {
+      this.logger.error('Database is offline', error);
+      return false;
+    }
+  }
+
+  async checkRedis(): Promise<boolean> {
+    try {
+      await this.client.ping();
+      return true;
+    } catch (error) {
+      this.logger.error('Redis is offline', error);
+      return false;
     }
   }
 }
