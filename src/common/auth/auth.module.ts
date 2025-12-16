7b4bc4f346550e0e3d64910f00abab0d0bc34b30
uxname <uxname@gmail.com>
2025-12-16 11:12:12 +0300

    Made prisma module global

--- src/common/auth/auth.module.ts
+++ src/common/auth/auth.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
-import { PrismaModule } from '@/common/prisma/prisma.module';
 import { JwtStrategy } from './jwt.strategy';
 import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Module({
-  imports: [PassportModule.register({ defaultStrategy: 'jwt' }), PrismaModule],
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   providers: [JwtStrategy, JwtAuthGuard],
   exports: [PassportModule, JwtAuthGuard],
 })
